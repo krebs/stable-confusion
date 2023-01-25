01717884ffc8c187c31f3466403b5e92b7074f8c
samularity <samuelmunz@gmx.de>
2023-01-25 20:31:51 +0100

    prevent screen lock

--- public/js/mediengewitter-client.js
+++ public/js/mediengewitter-client.js
@@ -18,7 +18,25 @@
       } catch (e) { }
     }
 
-    //const ws = new WebSocket('ws://localhost:8080')
+
+
+    async function noscreenlock() {
+      // Create a reference for the Wake Lock.
+      let wakeLock = null;
+
+      // create an async function to request a wake lock
+      try {
+        wakeLock = await navigator.wakeLock.request('screen');
+        statusElem.textContent = 'Wake Lock is active!';
+      } catch (err) {
+        // The Wake Lock request has failed - usually system related, such as battery.
+        statusElem.textContent = `${err.name}, ${err.message}`;
+      }
+    }
+
+    noscreenlock()
+
+    //const ws = new WebSocket('ws://localhost:8080')<
     const ws = new WebSocket(getWebSocketUri())
     ws.onopen = () => {
       console.log('ws opened on browser')
